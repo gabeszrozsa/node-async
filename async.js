bc53c85a88bfdb462bf3227e97cac9df16d278b7
Gabesz Rózsa <Gabesz Rzsa>
2017-05-04 09:58:59 +0200

    async

--- async.js
+++ async.js
@@ -4,15 +4,14 @@ const fetch = require("node-fetch");
 const async = require('asyncawait/async');
 const await = require('asyncawait/await');
 
-function showGitHubUser(handle) {
+var showGitHubUser = async (function (handle) {
   const url = `https://api.github.com/users/${handle}`;
 
-  fetch(url)
-    .then(response => response.json())
-    .then(user => {
-      console.log(user.name);
-      console.log(user.location);
-    });
-}
+  const response = await (fetch(url));
+  const user = await (response.json());
+  console.log(user.name);
+  console.log(user.location);
+
+});
 
 showGitHubUser("gabeszrozsa");
